aa12a4df2787aca742893ec8e0dfe238622d27f7
KW <k.wojtasinski@wp.pl>
2021-11-13 12:28:27 +0100

    Added refun repo

---
+++ ECommerceApp.Domain/Interface/IRefundRepository.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerceApp.Domain.Interface
+{
+    public interface IRefundRepository : IGenericRepository<Refund>
+    {
+        void DeleteRefund(int refundId);
+        int AddRefund(Refund refund);
+        Refund GetRefundById(int refundId);
+        IQueryable<Refund> GetAllRefunds();
+        void UpdateRefund(Refund refund);
+    }
+}
---
+++ ECommerceApp.Infrastructure/Repositories/RefundRepository.cs
@@ -0,0 +1,63 @@
+﻿using ECommerceApp.Domain.Interface;
+using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerceApp.Infrastructure.Repositories
+{
+    public class RefundRepository : GenericRepository<Refund>, IRefundRepository
+    {
+        public RefundRepository(Context context) : base(context)
+        {
+        }
+
+        public int AddRefund(Refund refund)
+        {
+            _context.Refunds.Add(refund);
+            _context.SaveChanges();
+            return refund.Id;
+        }
+
+        public void DeletePayment(int paymentId)
+        {
+            var payment = _context.Payments.Find(paymentId);
+
+            if (payment != null)
+            {
+                _context.Payments.Remove(payment);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteRefund(int refundId)
+        {
+            var refund = _context.Refunds.Find(refundId);
+
+            if (refund != null)
+            {
+                _context.Refunds.Remove(refund);
+                _context.SaveChanges();
+            }
+        }
+
+        public IQueryable<Refund> GetAllRefunds()
+        {
+            var refunds = _context.Refunds.AsQueryable();
+            return refunds;
+        }
+
+        public Refund GetRefundById(int refundId)
+        {
+            var refund = _context.Refunds.Where(p => p.Id == refundId).FirstOrDefault();
+            return refund;
+        }
+
+        public void UpdateRefund(Refund refund)
+        {
+            _context.Refunds.Update(refund);
+            _context.SaveChanges();
+        }
+    }
+}
