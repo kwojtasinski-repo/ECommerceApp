3e51b94c6fe994a2fc319b32e545a862cebfdd20
KW <k.wojtasinski@wp.pl>
2021-11-13 12:36:48 +0100

    Added tag repository

---
+++ ECommerceApp.Domain/Interface/ITagRepository.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerceApp.Domain.Interface
+{
+    public interface ITagRepository : IGenericRepository<Tag>
+    {
+        void DeleteTag(int tagId);
+        int AddTag(Tag tag);
+        Tag GetTagById(int tagId);
+        IQueryable<Tag> GetAllTags();
+        void UpdateTag(Tag tag);
+    }
+}
---
+++ ECommerceApp.Infrastructure/Repositories/TagRepository.cs
@@ -0,0 +1,52 @@
+﻿using ECommerceApp.Domain.Interface;
+using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerceApp.Infrastructure.Repositories
+{
+    public class TagRepository : GenericRepository<Tag>, ITagRepository
+    {
+        public TagRepository(Context context) : base(context)
+        {
+        }
+
+        public int AddTag(Tag tag)
+        {
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+            return tag.Id;
+        }
+
+        public void DeleteTag(int tagId)
+        {
+            var tag = _context.Tags.Find(tagId);
+
+            if (tag != null)
+            {
+                _context.Tags.Remove(tag);
+                _context.SaveChanges();
+            }
+        }
+
+        public IQueryable<Tag> GetAllTags()
+        {
+            var tags = _context.Tags.AsQueryable();
+            return tags;
+        }
+
+        public Tag GetTagById(int tagId)
+        {
+            var tag = _context.Tags.Where(t => t.Id == tagId).FirstOrDefault();
+            return tag;
+        }
+
+        public void UpdateTag(Tag tag)
+        {
+            _context.Tags.Update(tag);
+            _context.SaveChanges();
+        }
+    }
+}
