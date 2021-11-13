02004c640b3b82fdce58023e0dfed9c84d8b35dd
KW <k.wojtasinski@wp.pl>
2021-11-13 12:23:34 +0100

    Added payment repo

---
+++ ECommerceApp.Domain/Interface/IPaymentRepository.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerceApp.Domain.Interface
+{
+    public interface IPaymentRepository : IGenericRepository<Payment>
+    {
+        void DeletePayment(int paymentId);
+        int AddPayment(Payment payment);
+        Payment GetPaymentById(int paymentId);
+        IQueryable<Payment> GetAllPayments();
+        void UpdatePayment(Payment payment);
+    }
+}
---
+++ ECommerceApp.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,52 @@
+﻿using ECommerceApp.Domain.Interface;
+using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerceApp.Infrastructure.Repositories
+{
+    public class PaymentRepository : GenericRepository<Payment>, IPaymentRepository
+    {
+        public PaymentRepository(Context context) : base(context)
+        {
+        }
+
+        public int AddPayment(Payment payment)
+        {
+            _context.Payments.Add(payment);
+            _context.SaveChanges();
+            return payment.Id;
+        }
+
+        public void DeletePayment(int paymentId)
+        {
+            var payment = _context.Payments.Find(paymentId);
+
+            if (payment != null)
+            {
+                _context.Payments.Remove(payment);
+                _context.SaveChanges();
+            }
+        }
+
+        public IQueryable<Payment> GetAllPayments()
+        {
+            var payments = _context.Payments.AsQueryable();
+            return payments;
+        }
+
+        public Payment GetPaymentById(int paymentId)
+        {
+            var payment = _context.Payments.Where(p => p.Id == paymentId).FirstOrDefault();
+            return payment;
+        }
+
+        public void UpdatePayment(Payment payment)
+        {
+            _context.Payments.Update(payment);
+            _context.SaveChanges();
+        }
+    }
+}
