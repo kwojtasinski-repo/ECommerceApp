a1cd7f72c3c611357a475b73229961da5d97a304
KW <k.wojtasinski@wp.pl>
2021-11-13 11:58:04 +0100

    Added brand repository

---
+++ ECommerceApp.Domain/Interface/IBrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceApp.Domain.Model;
+using System.Linq;
+
+namespace ECommerceApp.Domain.Interface
+{
+    public interface IBrandRepository : IGenericRepository<Brand>
+    {
+        void DeleteBrand(int brandId);
+        int AddBrand(Brand brand);
+        Brand GetBrandById(int brandId);
+        IQueryable<Brand> GetAllBrands();
+        void UpdateBrand(Brand brand);
+    }
+}
---
+++ ECommerceApp.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,53 @@
+﻿using ECommerceApp.Domain.Interface;
+using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ECommerceApp.Infrastructure.Repositories
+{
+    public class BrandRepository : GenericRepository<Brand>, IBrandRepository
+    {
+        public BrandRepository(Context context) : base(context)
+        {
+        }
+
+        public int AddBrand(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+            return brand.Id;
+        }
+
+        public void DeleteBrand(int brandId)
+        {
+            var brand = _context.Brands.Find(brandId);
+
+            if (brand != null)
+            {
+                _context.Brands.Remove(brand);
+                _context.SaveChanges();
+            }
+        }
+
+        public IQueryable<Brand> GetAllBrands()
+        {
+            var brands = _context.Brands.AsQueryable();
+            return brands;
+        }
+
+        public Brand GetBrandById(int brandId)
+        {
+            var brand = _context.Brands.Where(b => b.Id == brandId).FirstOrDefault();
+            return brand;
+        }
+
+        public void UpdateBrand(Brand brand)
+        {
+            _context.Brands.Update(brand);
+            _context.SaveChanges();
+        }
+    }
+}
