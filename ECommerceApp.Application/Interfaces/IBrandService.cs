4c0bd43ee91502fd7e465e194039543055ea01c9
KW <k.wojtasinski@wp.pl>
2021-11-13 13:16:14 +0100

    Added Brand service

---
+++ ECommerceApp.Application/Interfaces/IBrandService.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceApp.Application.ViewModels.Brand;
+using ECommerceApp.Domain.Interface;
+using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ECommerceApp.Application.Interfaces
+{
+    public interface IBrandService : IAbstractService<BrandVm, IBrandRepository, Brand>
+    {
+        int AddBrand(BrandVm brandVm);
+        void DeleteBrand(int id);
+        ListForBrandVm GetAllBrands(int pageSize, int pageNo, string searchString);
+        BrandDetailsVm GetBrandDetail(int id);
+        BrandVm GetBrand(int id);
+        void UpdateBrand(BrandVm brandVm);
+        IEnumerable<BrandVm> GetAllBrands(Expression<Func<Brand, bool>> expression);
+    }
+}
---
+++ ECommerceApp.Application/Services/BrandService.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using ECommerceApp.Application.Abstracts;
+using ECommerceApp.Application.Exceptions;
+using ECommerceApp.Application.Interfaces;
+using ECommerceApp.Application.ViewModels.Brand;
+using ECommerceApp.Domain.Interface;
+using ECommerceApp.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ECommerceApp.Application.Services
+{
+    public class BrandService : AbstractService<BrandVm, IBrandRepository, Brand>, IBrandService
+    {
+        public BrandService(IBrandRepository brandRepo, IMapper mapper) : base(brandRepo, mapper)
+        { }
+
+        public int AddBrand(BrandVm brandVm)
+        {
+            if (brandVm.Id != 0)
+            {
+                throw new BusinessException("When adding object Id should be equals 0");
+            }
+            var id = Add(brandVm);
+            return id;
+        }
+
+        public void DeleteBrand(int id)
+        {
+            Delete(id);
+        }
+
+        public ListForBrandVm GetAllBrands(int pageSize, int pageNo, string searchString)
+        {
+            var brands = _repo.GetAllBrands().Where(it => it.Name.StartsWith(searchString))
+                .ProjectTo<BrandVm>(_mapper.ConfigurationProvider)
+                .ToList();
+            var brandsToShow = brands.Skip(pageSize * (pageNo - 1)).Take(pageSize).ToList();
+
+            var brandsList = new ListForBrandVm()
+            {
+                PageSize = pageSize,
+                CurrentPage = pageNo,
+                SearchString = searchString,
+                Brands = brandsToShow,
+                Count = brands.Count
+            };
+
+            return brandsList;
+        }
+
+        public BrandDetailsVm GetBrandDetail(int id)
+        {
+            var brand = _repo.GetAll().Include(i => i.Items).Where(b => b.Id == id).FirstOrDefault();
+            var brandDetails = _mapper.Map<BrandDetailsVm>(brand);
+            return brandDetails;
+        }
+
+        public BrandVm GetBrand(int id)
+        {
+            var brand = _repo.GetBrandById(id);
+            var brandVm = _mapper.Map<BrandVm>(brand);
+            return brandVm;
+        }
+
+        public void UpdateBrand(BrandVm brandVm)
+        {
+            Update(brandVm);
+        }
+
+        public IEnumerable<BrandVm> GetAllBrands(Expression<Func<Brand, bool>> expression)
+        {
+            var brands = _repo.GetAllBrands().Where(expression)
+                .ProjectTo<BrandVm>(_mapper.ConfigurationProvider);
+            var brandsToShow = brands.ToList();
+
+            return brandsToShow;
+        }
+    }
+}
