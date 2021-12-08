62a665c86d5d18230911170f3108a4d1c9760b4d
unknown <k.wojtasinski@wp.pl>
2021-12-08 22:39:41 +0100

    Added payment controller

---
+++ ECommerceApp.Web/Controllers/CurrencyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.Web.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
