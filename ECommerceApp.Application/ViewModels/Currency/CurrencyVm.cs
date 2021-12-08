5d6c6c9c766695a19cb11a43bd0e7235f78a9233
unknown <k.wojtasinski@wp.pl>
2021-12-08 22:36:37 +0100

    Added currencies Vm

---
+++ ECommerceApp.Application/ViewModels/Currency/CurrencyVm.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ECommerceApp.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceApp.Application.ViewModels.Currency
+{
+    public class CurrencyVm : BaseVm, IMapFrom<ECommerceApp.Domain.Model.Currency>
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CurrencyVm, ECommerceApp.Domain.Model.Currency>().ReverseMap();
+        }
+    }
+}
