d7b4bce232e26bc7498576671a8588bacbfb99f6
unknown <k.wojtasinski@wp.pl>
2021-12-18 00:03:11 +0100

    Added some test to order service

--- ECommerceApp.UnitTests/Services/Order/OrderServiceTests.cs
+++ ECommerceApp.UnitTests/Services/Order/OrderServiceTests.cs
@@ -1,4 +1,8 @@
-﻿using ECommerceApp.Application.Exceptions;
+﻿using AutoMapper;
+using ECommerceApp.Application.Exceptions;
+using ECommerceApp.Application.Interfaces;
+using ECommerceApp.Application.Mapping;
+using ECommerceApp.Application.Services;
 using ECommerceApp.Application.ViewModels.Order;
 using ECommerceApp.Domain.Interface;
 using ECommerceApp.Domain.Model;
@@ -6,53 +10,82 @@ using ECommerceApp.Infrastructure.Repositories;
 using ECommerceApp.Tests.Common;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
 
-namespace ECommerceApp.Tests.Services.OrderService
-{
-    public class OrderServiceTests// : BaseServiceTest<OrderDto, IOrderRepository, OrderRepository, Application.Services.OrderService, Order>
+namespace ECommerceApp.Tests.Services.Order
+{ 
+    public class OrderServiceTests
     {
-      /*  [Fact]
-        public void CanReturnOrder()
+        private readonly Mock<IOrderRepository> _orderRepository = new Mock<IOrderRepository>();
+        private readonly IMapper _mapper;
+        private readonly Mock<IOrderItemService> _orderItemService;
+        private readonly Mock<IItemService> _itemService;
+        private readonly Mock<ICouponService> _couponService;
+        private readonly Mock<ICouponUsedRepository> _couponUsedRepository;
+        private readonly Mock<ICustomerService> _customerService;
+
+        public OrderServiceTests()
         {
-            var id = 1;
-
-            var coupon = _service.Get(id);
-
-            coupon.Should().NotBeNull();
-            coupon.Should().BeOfType(typeof(OrderDto));
-        }
-
-        [Fact]
-        public void ShouldAddOrder()
-        {
-            var order = new OrderDto
+            var configurationProvider = new MapperConfiguration(cfg =>
             {
-                Id = 0,
-                CustomerId = 1,
-             //   IsPaid = true,
-              //  IsDelivered = false
-            };
+                cfg.AddProfile<MappingProfile>();
+            });
 
-            //var id = _service.Add(order);
-         //   var itemFromDb = _context.Orders.Where(i => i.Id == id).AsNoTracking().FirstOrDefault();
-
-         //   itemFromDb.Should().NotBeNull();
-         //   itemFromDb.Id.Should().Be(id);
+            _mapper = configurationProvider.CreateMapper();
+            _orderRepository = new Mock<IOrderRepository>();
+            _orderItemService = new Mock<IOrderItemService>();
+            _itemService = new Mock<IItemService>();
+            _couponService = new Mock<ICouponService>();
+            _couponUsedRepository = new Mock<ICouponUsedRepository>();
+            _customerService = new Mock<ICustomerService>();
         }
 
         [Fact]
-        public void ShouldntAddOrder()
+        public void given_invalid_order_when_dispatching_order_should_throw_an_exception()
         {
-            var order = new OrderDto { Id = 1000 };
+            var orderId = 1;
+            _orderRepository.Setup(o => o.GetAll()).Returns(new List<Domain.Model.Order>().AsQueryable());
+            var orderService = new OrderService(_orderRepository.Object, _mapper, _orderItemService.Object, _itemService.Object, _couponService.Object, _couponUsedRepository.Object, _customerService.Object);
+            var expectedException = new BusinessException($"Order with id {orderId} not found, check your order if is not delivered and is paid");
 
-           // Action act = () => _service.Add(order);
+            Action action = () => { orderService.DispatchOrder(orderId); };
 
-         //   act.Should().ThrowExactly<BusinessException>().WithMessage("When adding object Id should be equals 0");
-        }*/
+            action.Should().Throw<BusinessException>().WithMessage(expectedException.Message);
+        }
+
+        [Fact]
+        public void given_proper_order_when_dispatching_order_should_throw_an_exception()
+        {
+            var order = GetDefaultModel();
+            order.IsPaid = true;
+            _orderRepository.Setup(o => o.GetAll()).Returns(new List<Domain.Model.Order>() { order }.AsQueryable());
+            _orderRepository.Setup(o => o.Update(It.IsAny<Domain.Model.Order>())).Verifiable();
+            var orderService = new OrderService(_orderRepository.Object, _mapper, _orderItemService.Object, _itemService.Object, _couponService.Object, _couponUsedRepository.Object, _customerService.Object);
+
+            orderService.DispatchOrder(order.Id);
+
+            _orderRepository.Verify(o => o.Update(It.IsAny<Domain.Model.Order>()), Times.Once);
+        }
+
+        private Domain.Model.Order GetDefaultModel()
+        {
+            var order = new Domain.Model.Order();
+            order.Id = 1;
+            order.Number = 12345;
+            order.Cost = new decimal(100);
+            order.Ordered = DateTime.Now;
+            order.IsPaid = false;
+            order.IsDelivered = false;
+            order.Delivered = null;
+            order.CustomerId = 1;
+            order.CurrencyId = 1;
+            order.UserId = Guid.NewGuid().ToString();
+            return order;
+        }
     }
 }
