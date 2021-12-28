53a3d41b830e61cb0fc1d09a9819d00071f366d1
unknown <k.wojtasinski@wp.pl>
2021-12-28 20:58:58 +0100

    Added brandController tests

--- ECommerceApp.IntegrationTests/API/BrandControllerTests.cs
+++ ECommerceApp.IntegrationTests/API/BrandControllerTests.cs
@@ -17,14 +17,13 @@ using Xunit;
 
 namespace ECommerceApp.IntegrationTests.API
 {
-    public class BrandControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    public class BrandControllerTests : IClassFixture<BaseApiTest<Startup>>
     {
-        private readonly HttpClient _httpClient;
+        private readonly FlurlClient _client;
 
-        public BrandControllerTests(CustomWebApplicationFactory<Startup> fixture)
+        public BrandControllerTests(BaseApiTest<Startup> baseApiTest)
         {
-            fixture.Server.PreserveExecutionContext = true;
-            _httpClient = fixture.CreateClient();
+            _client = baseApiTest.Client;
         }
 
         [Fact]
@@ -33,22 +32,7 @@ namespace ECommerceApp.IntegrationTests.API
             var id = 1;
             var name = "Samsung";
 
-            var response = await _httpClient.GetAsync($"api/brands/{id}");
-            var brand = JsonConvert.DeserializeObject<BrandVm>(await response.Content.ReadAsStringAsync());
-
-            response.StatusCode.ShouldBe(HttpStatusCode.OK);
-            brand.ShouldNotBeNull();
-            brand.Name.ShouldBe(name);
-        }
-
-        [Fact]
-        public async Task given_valid_id_should_return_brand_with_flurl()
-        {
-            var id = 1;
-            var name = "Samsung";
-            using var client = new FlurlClient(_httpClient);
-
-            var response = await client.Request($"api/brands/{id}")
+            var response = await _client.Request($"api/brands/{id}")
                 .WithHeader("content-type", "application/json")
                 .AllowAnyHttpStatus()
                 .GetAsync();
@@ -57,9 +41,151 @@ namespace ECommerceApp.IntegrationTests.API
             response.StatusCode.ShouldBe((int) HttpStatusCode.OK);
             brand.ShouldNotBeNull();
             brand.Name.ShouldBe(name);
+        }
 
+        [Fact]
+        public async Task given_invalid_id_should_return_status_not_found()
+        {
+            var id = 21;
 
-            
+            var response = await _client.Request($"api/brands/{id}")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .GetAsync();
+
+            response.StatusCode.ShouldBe((int) HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task given_valid_brand_should_add()
+        {
+            var brand = CreateDefaultBrandVm(0);
+
+            var response = await _client.Request("api/brands")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .PostJsonAsync(brand);
+
+            response.StatusCode.ShouldBe((int) HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task given_invalid_brand_should_return_status_code_conflict()
+        {
+            var brand = CreateDefaultBrandVm(53);
+
+            var response = await _client.Request("api/brands")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .PostJsonAsync(brand);
+
+            response.StatusCode.ShouldBe((int) HttpStatusCode.Conflict);
+        }
+
+        [Fact]
+        public async Task given_valid_brand_should_update()
+        {
+            var id = 2;
+            var name = "TestBrand";
+            var brand = await _client.Request($"api/brands/{id}")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .GetJsonAsync<BrandVm>();
+            brand.Name = name;
+
+            var response = await _client.Request("api/brands")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .PutJsonAsync(brand);
+
+            var brandUpdated = await _client.Request($"api/brands/{id}")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .GetJsonAsync<BrandVm>();
+            response.StatusCode.ShouldBe((int) HttpStatusCode.OK);
+            brandUpdated.Name.ShouldBe(name);
+        }
+
+        [Fact]
+        public async Task given_not_existed_brand_should_return_status_code_conflict()
+        {
+            var id = 223;
+            var brand = CreateDefaultBrandVm(id);
+
+            var response = await _client.Request("api/brands")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .PutJsonAsync(brand);
+
+            response.StatusCode.ShouldBe((int) HttpStatusCode.Conflict);
+        }
+
+        [Fact]
+        public async Task given_valid_id_should_delete_brand()
+        {
+            var brand = CreateDefaultBrandVm(0);
+            var id = await _client.Request("api/brands")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .PostJsonAsync(brand)
+                .ReceiveJson<int>();
+
+            var response = await _client.Request($"api/brands/{id}")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .DeleteAsync();
+
+            var responseAfterUpdate = await _client.Request($"api/brands/{id}")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .GetAsync();
+            response.StatusCode.ShouldBe((int) HttpStatusCode.OK);
+            responseAfterUpdate.StatusCode.ShouldBe((int) HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task given_brands_in_db_should_return_brands()
+        {
+            var brands = await _client.Request($"api/brands")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .GetJsonAsync<List<BrandVm>>();
+
+            brands.Count.ShouldBeGreaterThan(0);
+            brands.Count.ShouldBe(3);
+        }
+
+        [Fact]
+        public async Task given_no_brands_should_return_status_code_not_found()
+        {
+            _ = DeleteBrand(1);
+            _ = DeleteBrand(2);
+            await DeleteBrand(3);
+
+            var brands = await _client.Request($"api/brands")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .GetAsync();
+
+            brands.StatusCode.ShouldBe((int) HttpStatusCode.NotFound);
+        }
+
+        private Task DeleteBrand(int id)
+        {
+            return _client.Request($"api/brands/{id}")
+                .WithHeader("content-type", "application/json")
+                .AllowAnyHttpStatus()
+                .DeleteAsync();
+        }
+
+        private BrandVm CreateDefaultBrandVm(int id)
+        {
+            var brand = new BrandVm
+            {
+                Id = id,
+                Name = "BrandTest"
+            };
+            return brand;
         }
     }
 }
