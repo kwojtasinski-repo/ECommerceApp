7d524742c21a773e98b6ab0861f86da03407d8cd
KW <k.wojtasinski@wp.pl>
2023-10-05 19:29:30 +0200

    [Frontend] Improve modals, added button template

--- ECommerceApp.Web/wwwroot/js/site.js
+++ ECommerceApp.Web/wwwroot/js/site.js
@@ -2,34 +2,70 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+const buttonTemplate = (function () {
+    return {
+        /**
+         * Creates button template
+         * @param elementName Text of button.
+         * @param className Class button.
+         * @param onclick Method invoked on click.
+         * @param type Type of button.
+         * @param attributes Attributes of button. Template [{ key: value }].
+         *
+         */
+        createButton: function (text, className, onclick, type, attributes) {
+            const button = document.createElement("button");
+            button.textContent = text ? text : "";
+            button.className = className ? className : "";
+            button.onclick = onclick ? onclick : () => { };
+            button.type = type ? type : "button";
+
+            if (attributes) {
+                for (const attribute of attributes) {
+                    if (!attribute || !attribute.key || !attribute.value) {
+                        continue;
+                    }
+
+                    button.setAttribute(attribute.key, attribute.value);
+                }
+            }
+
+            return button;
+        }
+    }
+})();
+
 const modalService = (function () {
     // private variables, functions
     const modalIdentifyClass = "modalClass";
 
-    function createModalTemplate(headerText, bodyText) {
+    function createModalTemplate(headerTemplate, bodyTemplate, footerTemplate) {
         const modalDiv = document.createElement("div");
         modalDiv.className = "modal fade " + modalIdentifyClass;
         modalDiv.tabIndex = "-1";
         modalDiv.role = "dialog";
-        modalDiv.appendChild(createDialog(headerText, bodyText));
+        modalDiv.appendChild(createDialog(headerTemplate, bodyTemplate, footerTemplate));
         return modalDiv;
     }
 
-    function createDialog(headerText, bodyText) {
+    function createDialog(headerTemplate, bodyTemplate, footerTemplate) {
         const modalDialog = document.createElement("div");
         modalDialog.className = "modal-dialog modal-dialog-centered";
         modalDialog.role = "document";
-        modalDialog.appendChild(createModalContent(headerText, bodyText));
+        modalDialog.appendChild(createModalContent(headerTemplate, bodyTemplate, footerTemplate));
         return modalDialog;
     }
 
-    function createModalContent(headerText, bodyText) {
+    function createModalContent(headerTemplate, bodyTemplate, footerTemplate) {
         const modalContent = document.createElement("div");
         modalContent.className = "modal-content";
 
-        modalContent.appendChild(createModalHeader(headerText));
-        modalContent.appendChild(createModalBody(bodyText));
-        modalContent.appendChild(createModalFooter());
+        modalContent.appendChild(headerTemplate);
+        modalContent.appendChild(bodyTemplate);
+        modalContent.appendChild(footerTemplate);
 
         return modalContent;
     }
@@ -45,12 +81,7 @@ const modalService = (function () {
             modalHeader.appendChild(titleText);
         }
 
-        const closeButton = document.createElement("button");
-        closeButton.type = "button";
-        closeButton.className = "close";
-        closeButton.setAttribute("data-dismiss", "modal");
-        closeButton.setAttribute("aria-label", "Close");
-        closeButton.onclick = closeModal;
+        const closeButton = buttonTemplate.createButton(undefined, "close", closeModal, "button", [{ key: "data-dismiss", value: "modal" }, { key: "aria-label", value: "Close" }]);
 
         const spanCloseButton = document.createElement("span");
         spanCloseButton.setAttribute("aria-hidden", "true");
@@ -73,15 +104,18 @@ const modalService = (function () {
         return modalBody;
     }
 
-    function createModalFooter() {
+    function createModalFooter(buttons) {
         const modalFooter = document.createElement("div");
         modalFooter.className = "modal-footer";
-        const closeButton = document.createElement("button");
-        closeButton.className = "btn btn-secondary";
-        closeButton.type = "button";
-        closeButton.setAttribute("data-dismiss", "modal");
-        closeButton.textContent = "Close";
-        closeButton.onclick = closeModal;
+
+        if (buttons) {
+            for (const button of buttons) {
+                modalFooter.appendChild(button);
+            }
+            return modalFooter;
+        }
+
+        const closeButton = buttonTemplate.createButton("Close", "btn btn-secondary", closeModal, "button", [{ key: "data-dismiss", value: "modal" }]);
         modalFooter.appendChild(closeButton);
 
         return modalFooter;
@@ -100,12 +134,26 @@ const modalService = (function () {
 
     // public variables functions
     return {
-        show: function (header, body) {
-            const modalTemplate = createModalTemplate(header, body);
+        showInformationModal: function (headerText, bodyText) {
+            debugger
+            const headerTemplate = createModalHeader(headerText);
+            const bodyTemplate = createModalBody(bodyText);
+            const footerTemplate = createModalFooter();
+            const modalTemplate = createModalTemplate(headerTemplate, bodyTemplate, footerTemplate);
             document.body.appendChild(modalTemplate);
             $('.' + modalIdentifyClass).modal('show');
         },
-        hide: function () {
+        showConfirmationModal: function (headerText, bodyText, result) {
+            const headerTemplate = createModalHeader(headerText);
+            const bodyTemplate = createModalBody(bodyText);
+            const confirmButton = buttonTemplate.createButton("Yes", "btn btn-danger", () => { this.close();result(); }, "type");
+            const cancelButton = buttonTemplate.createButton("No", "btn btn-secondary", this.close, "type");
+            const footerTemplate = createModalFooter([confirmButton, cancelButton]);
+            const modalTemplate = createModalTemplate(headerTemplate, bodyTemplate, footerTemplate);
+            document.body.appendChild(modalTemplate);
+            $('.' + modalIdentifyClass).modal('show');
+        },
+        close: function () {
             closeModal();
         }
     }
