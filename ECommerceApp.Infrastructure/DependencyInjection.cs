d2878f9875383f42f1e1388be849b5007730509a
KW <k.wojtasinski@wp.pl>
2023-11-13 19:12:27 +0100

    [Backend] Improve

--- ECommerceApp.Infrastructure/DependencyInjection.cs
+++ ECommerceApp.Infrastructure/DependencyInjection.cs
@@ -11,28 +11,9 @@ namespace ECommerceApp.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddTransient<ICustomerRepository, CustomerRepository>();
-            services.AddTransient<IItemRepository, ItemRepository>();
-            services.AddTransient<IOrderRepository, OrderRepository>();
-            services.AddTransient<ICouponRepository, CouponRepository>();
-            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            services.AddTransient<IImageRepository, ImageRepository>();
-            services.AddTransient<ICouponTypeRepository, CouponTypeRepository>();
-            services.AddTransient<ICouponUsedRepository, CouponUsedRepository>();
-            services.AddTransient<IBrandRepository, BrandRepository>();
-            services.AddTransient<IPaymentRepository, PaymentRepository>();
-            services.AddTransient<IRefundRepository, RefundRepository>();
-            services.AddTransient<ITagRepository, TagRepository>();
-            services.AddTransient<ITypeRepository, TypeRepository>();
-            services.AddTransient<IOrderItemRepository, OrderItemRepository>();
-            services.AddTransient<IContactDetailRepository, ContactDetailRepository>();
-            services.AddTransient<IContactDetailTypeRepository, ContactDetailTypeRepository>();
-            services.AddTransient<IAddressRepository, AddressRepository>();
-            services.AddTransient<ICurrencyRepository, CurrencyRepository>();
-            services.AddTransient<ICurrencyRateRepository, CurrencyRateRepository>();
             services.AddDatabase(configuration);
+            services.AddRepositories();
             services.AddAuth(configuration);
-
             return services;
         }
     }
---
+++ ECommerceApp.Infrastructure/Repositories/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using ECommerceApp.Domain.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerceApp.Infrastructure.Repositories
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IItemRepository, ItemRepository>();
+            services.AddTransient<IOrderRepository, OrderRepository>();
+            services.AddTransient<ICouponRepository, CouponRepository>();
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IImageRepository, ImageRepository>();
+            services.AddTransient<ICouponTypeRepository, CouponTypeRepository>();
+            services.AddTransient<ICouponUsedRepository, CouponUsedRepository>();
+            services.AddTransient<IBrandRepository, BrandRepository>();
+            services.AddTransient<IPaymentRepository, PaymentRepository>();
+            services.AddTransient<IRefundRepository, RefundRepository>();
+            services.AddTransient<ITagRepository, TagRepository>();
+            services.AddTransient<ITypeRepository, TypeRepository>();
+            services.AddTransient<IOrderItemRepository, OrderItemRepository>();
+            services.AddTransient<IContactDetailRepository, ContactDetailRepository>();
+            services.AddTransient<IContactDetailTypeRepository, ContactDetailTypeRepository>();
+            services.AddTransient<IAddressRepository, AddressRepository>();
+            services.AddTransient<ICurrencyRepository, CurrencyRepository>();
+            services.AddTransient<ICurrencyRateRepository, CurrencyRateRepository>();
+            return services;
+        }
+    }
+}
