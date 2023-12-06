1195a1eae39a1c195d87ed1b937b13de8e29d9ad
KW <k.wojtasinski@wp.pl>
2023-12-06 20:47:32 +0100

    [Backend] Introduce ContactDetailTypeDto

---
+++ ECommerceApp.Application/DTO/ContactDetailTypeDto.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ECommerceApp.Application.Mapping;
+using FluentValidation;
+
+namespace ECommerceApp.Application.DTO
+{
+    public class ContactDetailTypeDto : IMapFrom<Domain.Model.ContactDetail>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Model.ContactDetailType, ContactDetailTypeDto>().ReverseMap();
+        }
+    }
+
+    public class ContactDetailTypeDtoValidation : AbstractValidator<ContactDetailTypeDto>
+    {
+        public ContactDetailTypeDtoValidation()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Name).NotNull();
+        }
+    }
+}
--- ECommerceApp.Application/Services/ContactDetails/IContactDetailTypeService.cs
+++ ECommerceApp.Application/Services/ContactDetails/IContactDetailTypeService.cs
@@ -5,7 +5,6 @@ using ECommerceApp.Domain.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace ECommerceApp.Application.Services.ContactDetails
 {
