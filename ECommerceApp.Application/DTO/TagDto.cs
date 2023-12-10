74ceeeefe48fabf595443ec67f96a9c54ca94ba3
KW <k.wojtasinski@wp.pl>
2023-12-10 20:00:42 +0100

    [Backend] TagDto Introduce

---
+++ ECommerceApp.Application/DTO/TagDto.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using ECommerceApp.Application.Mapping;
+using ECommerceApp.Domain.Model;
+using FluentValidation;
+
+namespace ECommerceApp.Application.DTO
+{
+    public class TagDto : IMapFrom<Tag>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<TagDto, Tag>()
+               .ReverseMap();
+        }
+    }
+
+    public class TagDtoValidator : AbstractValidator<TagDto>
+    {
+        public TagDtoValidator()
+        {
+            RuleFor(b => b.Name).NotEmpty()
+                    .MinimumLength(2)
+                    .MaximumLength(100);
+        }
+    }
+}
