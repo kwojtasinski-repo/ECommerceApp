c861da70b4c6f4f7349e081c1916ff766e1168ae
KW <k.wojtasinski@wp.pl>
2023-12-17 18:03:29 +0100

    [Backend] TypeDto Introduce

---
+++ ECommerceApp.Application/DTO/TypeDto.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace ECommerceApp.Application.DTO
+{
+    public class TypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TypeDtoValidator : AbstractValidator<TypeDto>
+    {
+        public TypeDtoValidator()
+        {
+            RuleFor(b => b.Name).NotEmpty()
+                    .MinimumLength(2)
+                    .MaximumLength(100);
+        }
+    }
+}
