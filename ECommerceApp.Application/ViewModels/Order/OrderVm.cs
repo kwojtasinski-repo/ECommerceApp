7bdc9a9bfe959679d6cfa4ccd1fb032b85e9bfe5
KW <k.wojtasinski@wp.pl>
2024-02-04 13:47:05 +0100

    [Backend] [Frontend] Bugfixing

--- ECommerceApp.Application/ViewModels/Order/OrderVm.cs
+++ ECommerceApp.Application/ViewModels/Order/OrderVm.cs
@@ -9,7 +9,7 @@ namespace ECommerceApp.Application.ViewModels.Order
     {
         public OrderDto Order { get; set; }
         public List<CustomerInformationForOrdersVm> Customers { get; set; } = new List<CustomerInformationForOrdersVm>();
-        public CustomerDetailsDto NewCustomer { get; internal set; }
+        public CustomerDetailsDto NewCustomer { get; set; }
         public bool CustomerData { get; set; }
         public string PromoCode { get; set; }
     }
@@ -22,16 +22,6 @@ namespace ECommerceApp.Application.ViewModels.Order
             {
                 RuleFor(c => c.NewCustomer).NotNull().SetValidator(new CustomerDetailsDtoValidator());
             });
-            When(o => o.CustomerData, () =>
-            {
-                RuleFor(c => c.NewCustomer).Null();
-            });
-        }
-
-        public override ValidationResult Validate(ValidationContext<OrderVm> context)
-        {
-            var result = base.Validate(context);
-            return result;
         }
     }
 }
--- ECommerceApp.Web/Filters/ModelStateFilter.cs
+++ ECommerceApp.Web/Filters/ModelStateFilter.cs
@@ -1,7 +1,5 @@
-﻿using ECommerceApp.Infrastructure.Database;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ECommerceApp.Web.Filters
 {
@@ -9,29 +7,11 @@ namespace ECommerceApp.Web.Filters
     {
         public void OnActionExecuting(ActionExecutingContext context)
         {
-            SetUnvalidatedAsValidated(context.ModelState);
             if (!context.ModelState.IsValid)
             {
                 context.Result = new BadRequestObjectResult(context.ModelState);
             }
         }
         public void OnActionExecuted(ActionExecutedContext context) { }
-
-        // Bug FluentValidation cannot set values if skipped as validated
-        private static void SetUnvalidatedAsValidated(ModelStateDictionary modelState)
-        {
-            if (modelState.ValidationState != ModelValidationState.Unvalidated)
-            {
-                return;
-            }
-
-            foreach (var e in modelState)
-            {
-                if (modelState[e.Key].ValidationState == ModelValidationState.Unvalidated)
-                {
-                    modelState[e.Key].ValidationState = ModelValidationState.Valid;
-                }
-            }
-        }
     }
 }
