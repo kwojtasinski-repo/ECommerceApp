32f1c0306b0fa35ae42f0f55d41e9309c85b8eb6
KW <k.wojtasinski@wp.pl>
2026-02-27 22:23:12 +0100

    [Backend] [Currencies] Added missing migration

---
+++ ECommerceApp.Infrastructure/Supporting/Currencies/Migrations/20260227212141_InitCurrencySchema.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceApp.Infrastructure.Supporting.Currencies.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCurrencySchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "currencies");
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                schema: "currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CurrencyRates",
+                schema: "currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrencyId = table.Column<int>(type: "int", nullable: false),
+                    Rate = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false),
+                    CurrencyDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CurrencyRates_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalSchema: "currencies",
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Currencies_Code",
+                schema: "currencies",
+                table: "Currencies",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrencyRates_CurrencyId",
+                schema: "currencies",
+                table: "CurrencyRates",
+                column: "CurrencyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyRates",
+                schema: "currencies");
+
+            migrationBuilder.DropTable(
+                name: "Currencies",
+                schema: "currencies");
+        }
+    }
+}
